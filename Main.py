f957d60bf45832f69e49531bfc70c38eab0ccd66
Minikitteh <yamel0727@gmail.com>
2018-09-13 22:03:00 -0600

    first commit to code

---
+++ Main.py
@@ -0,0 +1,53 @@
+import os
+import random
+
+
+def get_dirs_and_files(path):
+    dir_list = [directory for directory in os.listdir(path) if os.path.isdir(path + '/' + directory)]
+    file_list = [directory for directory in os.listdir(path) if not os.path.isdir(path + '/' + directory)]
+
+    return dir_list, file_list
+
+
+def classify_pic(path):
+    # To be implemented by Diego: Replace with ML model
+    if "dog" in path:
+        return 0.5 + random.random() / 2
+
+    return random.random() / 2
+
+
+##############################################################
+def process_dir(path):
+    dir_list, file_list = get_dirs_and_files(path)
+
+    cat_list = []
+    dog_list = []
+
+    #my code
+    for fPath in file_list:
+        if classify_pic(fPath) > .5:
+            dog_list.append(fPath)
+        else:
+            cat_list.append(fPath)
+    
+    if not dir_list:
+        #print("in the thing")
+        return cat_list, dog_list
+    
+    for dL in dir_list:
+        print(str(path+"/"+dL))
+        cl, dl = process_dir(str(path+"/"+dL))
+        cat_list.extend(cl)
+        dog_list.extend(dl)
+
+    return cat_list, dog_list
+###############################################################
+
+def main():
+    start_path = '/home/yamel/Desktop/CS3/1.1 CatsDogs' # current directory
+
+    process_dir(start_path)
+
+
+main()
